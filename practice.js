828cfa9f065ee6ce550b28dac116a7f85dfba3ba
kaya0909 <alecjr0909@gmail.com>
2021-11-28 23:29:18 +0900

    연구 원칙 애니메이션 추가

--- practice.js
+++ practice.js
@@ -5,3 +5,12 @@ function come_back() {
 function alert_() {
   alert("지금은 입학 상담 및 지원이 불가합니다.");
 }
+
+function otaku() {
+  if(confirm("정말 이곳으로 들어오시겠습니까?")) {
+    alert("돌아오신걸 환영합니다")
+  }
+  else {
+    alert("시간이 지난 후 다시 찾아와주세요")
+  }
+}
